89949ebd88cb4f45ced4017b8e7c74ad5b2f3e72
Holly Leary <schleary@gmail.com>
2014-12-02 16:59:50 -0800

    user controller rspec tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, :type => :controller do
+
+  describe "GET 'account'" do
+    it "is successful" do
+      get :account, id: 1
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "POST 'account'" do
+    it "is successful" do
+      post :account, id: 1
+      expect(response.status).to eq(200)
+    end
+  end
+  
+
+end
