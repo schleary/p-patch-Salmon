18ef3194765ff11c76615391a4ae668ed79100d6
Holly Leary <schleary@gmail.com>
2014-12-05 09:13:32 -0800

    sessions progress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,24 @@
 class SessionsController < ApplicationController
   def new
+    create
   end
 
   def create
-    puts request.env['omniauth.auth'].inspect
     auth_hash = request.env['omniauth.auth']
+    puts "********"
+    puts auth_hash.inspect
 
-    user = User.find_by_uid(auth_hash[:uid])
+    user = User.find_by_uid(auth_hash.uid)
     if user && user.authenticate()
       session[:user_id] = user.id
       redirect_to root_url
     else
       render 'new'
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
