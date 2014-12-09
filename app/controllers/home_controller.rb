283c41f40ab783e834bf3622f4021e177c114935
Holly Leary <schleary@gmail.com>
2014-12-09 11:17:10 -0800

    weather class

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,8 @@
 # This is the home controller. It controls the index page.
 class HomeController < ApplicationController
+
+  def index
+    @w = Weather.query
+  end
+
 end
--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,8 +1,19 @@
 class Weather < ActiveRecord::Base
-
+  
   def query
     @response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Seattle,%20wa")
     parse(@response)
   end
-  
+
+  def parse response
+    @weather = Weather.new(temp:               response['main']['temp'],
+                humidity:           response['main']['humidity'],
+                current_conditions: response['weather']['main'],
+                description:        response['weather']['description'],
+                weather_icon:       response['weather']['icon'],
+                windspeed:          ressponse['wind']['speed']
+                )
+    @weather.save
+  end
+
 end
