459621e8593da72e02fbe74f6e054b1d92fcfb8c
Linnea <lkdamer@gmail.com>
2014-12-09 16:09:24 -0800

    temp conversion in header stuff

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,7 +3,7 @@ class Weather < ActiveRecord::Base
   def self.query
     response = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?q=Seattle,%20wa').parsed_response
 
-    z = { temp:                response['main']['temp'],
+    z = { temp:                sprintf("%.2f", ( response['main']['temp'] * 9 / 5) - 459.67),
           humidity:            response['main']['humidity'],
           current_conditions:  response['weather'][-1]['main'],
           description:         response['weather'][-1]['description'],
