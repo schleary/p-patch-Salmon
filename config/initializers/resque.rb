8f7a2f0817fee57d0fb803eb6fc8bcff4e542b3e
Holly Leary <schleary@gmail.com>
2014-12-12 15:17:44 -0800

    working on worker for heroku

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,8 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+
+HerokuResqueAutoscaler.configure do |config|
+  config.heroku_api_key  = ENV["HEROKU_API_KEY"]
+  config.heroku_app_name = ENV["HEROKU_APP_NAME"]
+end
